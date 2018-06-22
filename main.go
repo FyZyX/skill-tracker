e79bfe7dfdd25d1d58d177a75c8b088c7096b241
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-06-22 00:07:15 -0700

    Use request method to determine routing

--- main.go
+++ main.go
@@ -9,18 +9,25 @@ import (
 var tpl *template.Template
 
 func init() {
+	// Parse templates
 	tpl = template.Must(template.ParseFiles("templates/index.gohtml"))
 }
 
 func main() {
+	// Define route handlers
 	http.Handle("/assets/", http.StripPrefix("/assets", http.FileServer(http.Dir("./assets"))))
 	http.Handle("/favicon.ico", http.NotFoundHandler())
 	http.HandleFunc("/", indexRoute)
 
+	// Start HTTP server
 	log.Printf("Listening on port 8080")
 	log.Fatal(http.ListenAndServe(":8080", nil))
 }
 
 func indexRoute(w http.ResponseWriter, r *http.Request) {
-	tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	switch r.Method {
+	// Handle GET requests
+	case http.MethodGet:
+		tpl.ExecuteTemplate(w, "index.gohtml", nil)
+	}
 }
