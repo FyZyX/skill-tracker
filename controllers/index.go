f5e448b2129278ac4f450e723fadd4d63a26de84
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-06-25 22:58:29 -0700

    Pass skill object in template data

--- controllers/index.go
+++ controllers/index.go
@@ -1,16 +1,20 @@
 package controllers
 
-import "net/http"
+import (
+	"net/http"
+	"skillTracker/models"
+)
 
 func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	// Handle GET requests
 	case http.MethodGet:
-		data := struct {
-			Mastery int
+		data := struct{
+			Skill models.Skill
 		}{
-			25,
+			models.NewSkill("Single digit addition", 0.31),
 		}
+
 		views.ExecuteTemplate(w, routes.index, data)
 	}
 }
