84f92b269ec5574a882bb9fb48bbcc23da2da454
Lucas Lofaro <lucasmlofaro@gmail.com>
2018-06-27 23:53:50 -0700

    Display skill list

--- controllers/index.go
+++ controllers/index.go
@@ -9,10 +9,16 @@ func IndexHandler(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	// Handle GET requests
 	case http.MethodGet:
-		data := struct{
-			Skill models.Skill
-		}{
+		skills := []models.Skill{
 			models.NewSkill("Single digit addition", 0.31),
+			models.NewSkill("Double digit addition", 0.43),
+			models.NewSkill("Single digit subtraction", 0.56),
+			models.NewSkill("Single digit multiplication", 0.27),
+		}
+		data := struct{
+			Skills []models.Skill
+		}{
+			skills,
 		}
 
 		views.ExecuteTemplate(w, routes.index, data)
